ce7d39143bc50b97e104ceffadd5d4868b2abd6e
Igor William <igorwm9@gmail.com>
2019-04-17 09:46:49 -0300

    Updates

--- precache-manifest.6091cdd5e65d23ab58ff9c5c3445bab1.js
+++ precache-manifest.6091cdd5e65d23ab58ff9c5c3445bab1.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/oministack-frontend/static/js/runtime~main.94352f9c.js"
   },
   {
-    "revision": "8d266c1d2ffc4552127c",
-    "url": "/oministack-frontend/static/js/main.fc931cdb.chunk.js"
+    "revision": "3553a2b196a4c0fdb431",
+    "url": "/oministack-frontend/static/js/main.19b8f8ce.chunk.js"
   },
   {
     "revision": "ba2a384e0243e89afc04",
     "url": "/oministack-frontend/static/js/2.bbeb62c1.chunk.js"
   },
   {
-    "revision": "8d266c1d2ffc4552127c",
-    "url": "/oministack-frontend/static/css/main.3a0b417c.chunk.css"
+    "revision": "3553a2b196a4c0fdb431",
+    "url": "/oministack-frontend/static/css/main.3a5dbbf6.chunk.css"
   },
   {
-    "revision": "98e41b30388155c3ad464f1d650f1071",
+    "revision": "38209a1119812187f9b9954093a918d1",
     "url": "/oministack-frontend/index.html"
   }
 ];
